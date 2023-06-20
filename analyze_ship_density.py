df94dae001bff8345893978c46a1dc6cd51cf02a
Nikos Benas <74472458+nikosbenas@users.noreply.github.com>
2023-06-20 11:16:09 +0200

    Read CLAAS-3 lat and lon

--- analyze_ship_density.py
+++ analyze_ship_density.py
@@ -10,6 +10,12 @@ import numpy as np
 import sys
 sys.path.append('/usr/people/benas/Documents/CMSAF/python_modules/')
 from modis_python_functions import map_l3_var as create_map
+sys.path.append('/data/windows/m/benas/Documents/CMSAF/CLAAS-3/CLAAS-3_trends')
+import claas_trends_functions as ctf
+import claas3_dictionaries as cdict
+import h5py
+from datetime import datetime 
+from netCDF4 import Dataset
 
 
 def get_rowcol_from_latlon(lat, lon, filename):
@@ -87,17 +93,17 @@ def sum_grid_cells(array, block_size):
     """
     
     # Calculate the number of blocks in each dimension
-   num_blocks_row = array.shape[0] // block_size
-   num_blocks_col = array.shape[1] // block_size
+    num_blocks_row = array.shape[0] // block_size
+    num_blocks_col = array.shape[1] // block_size
    
-   # Reshape the array into blocks of the specified size
-   blocks = array[:num_blocks_row * block_size, :num_blocks_col * block_size]
-   blocks = blocks.reshape(num_blocks_row, block_size, num_blocks_col, block_size)
+    # Reshape the array into blocks of the specified size
+    blocks = array[:num_blocks_row * block_size, :num_blocks_col * block_size]
+    blocks = blocks.reshape(num_blocks_row, block_size, num_blocks_col, block_size)
    
-   # Calculate the sum of values within each block
-   block_sums = np.sum(blocks, axis=(1, 3))
+    # Calculate the sum of values within each block
+    block_sums = np.sum(blocks, axis=(1, 3))
    
-   return block_sums
+    return block_sums
 
 
 def average_grid_cells(array, block_size):
@@ -112,17 +118,17 @@ def average_grid_cells(array, block_size):
     """
     
     # Calculate the number of blocks in each dimension
-   num_blocks_row = array.shape[0] // block_size
-   num_blocks_col = array.shape[1] // block_size
+    num_blocks_row = array.shape[0] // block_size
+    num_blocks_col = array.shape[1] // block_size
    
-   # Reshape the array into blocks of the specified size
-   blocks = array[:num_blocks_row * block_size, :num_blocks_col * block_size]
-   blocks = blocks.reshape(num_blocks_row, block_size, num_blocks_col, block_size)
+    # Reshape the array into blocks of the specified size
+    blocks = array[:num_blocks_row * block_size, :num_blocks_col * block_size]
+    blocks = blocks.reshape(num_blocks_row, block_size, num_blocks_col, block_size)
    
-   # Calculate the sum of values within each block
-   block_averages = np.mean(blocks, axis=(1, 3))
+    # Calculate the sum of values within each block
+    block_averages = np.mean(blocks, axis=(1, 3))
     
-   return block_averages
+    return block_averages
 
 
 # =============================================================================
@@ -154,7 +160,7 @@ data = src.read(1, window = ((rows[0], rows[3]), (cols[0], cols[1])))
 
 src.close()
 
-del rows, cols, geotiff_filename, lats, lons, src, stride, c, i, r
+del rows, cols, geotiff_filename, src, stride, c, i, r
 
 # =============================================================================
 # Reduce lat, lon and ship data resolution to match the CLAAS-3 resolution 
@@ -167,10 +173,60 @@ data_reduced = sum_grid_cells(data, block_size)
 lat_reduced = average_grid_cells(lat, block_size)
 lon_reduced = average_grid_cells(lon, block_size)
 
+# =============================================================================
+# Read CLAAS-3 data
+# =============================================================================
+
+# Define bounding box: ul lon, lr lat,lr lon, ul lat
+bounding_box = [lons[0], lats[2], lons[2], lats[0]]
+stride = 1
+
+# Define variables to read
+var_list = ['cdnc_liq']
+
+# Select the time period to analyze CLAAS-3 data
+start_year = 2004
+end_year = 2022
+
+# Create vector of dates for plotting
+dates = [datetime.strptime(str(year) + str(month).zfill(2) + '01', '%Y%m%d')
+         for year in range(start_year, end_year + 1) for month in range(1, 13)]
+
+# Read full CLAAS-3 lat and lon arrays, needed to find b.box indices
+claas3_l3_aux_data_file = '/data/windows/m/benas/Documents/CMSAF/CLAAS-3/' +\
+    'CLAAS-3_trends/claas3_level3_aux_data_005deg.nc'
+f = h5py.File(claas3_l3_aux_data_file, 'r')
+latData = f['lat'][:]
+lonData = f['lon'][:]
+f.close()
+lonData, latData = np.meshgrid(lonData, latData)
+
+istart, iend, jstart, jend = ctf.find_bbox_indices(bounding_box, latData,
+                                                   lonData)
+
+del latData, lonData 
+
+for var in var_list:
+    
+    if 'lat_claas' not in locals():
+          
+          # Use an existing file
+        claas3_file = cdict.cdr_folder[2004] + '2004/02/' +\
+            cdict.FileNameStart[var] + '20040201000000423SVMSG01MA.nc'
+        # Open the file and read the latitude and longitude
+        claas3_data = Dataset(claas3_file, 'r')
+        lat_claas = claas3_data['lat'][istart:iend:stride]
+        lon_claas = claas3_data['lon'][jstart:jend:stride]
+        # Create mesh grid
+        lon_claas, lat_claas = np.meshgrid(lon_claas, lat_claas)
+        
+        del claas3_file, claas3_data
+
 outfile = './testmap_SE_Atlantic_reduced_resolution.png'
 
-create_map(lat_reduced, lon_reduced, data_reduced, np.min(data_reduced), np.max(data_reduced), 'test', '-', 'Reds',
-           'neither', outfile, saveplot = True)
+create_map(lat_reduced, lon_reduced, data_reduced, np.min(data_reduced), 
+           np.max(data_reduced), 'test', '-', 'Reds', 'neither', outfile, 
+           saveplot = True)
 
 # Access the dataset attributes
 # print(src.crs)        # Coordinate reference system
